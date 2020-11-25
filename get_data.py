b3d1a7f42b5083c3d237e59bdf4f232eb3a1eb5d
Sparrow-zhu <59820895+Sparrow-zhu@users.noreply.github.com>
2020-11-25 16:06:57 +0800

    Add files via upload

---
+++ get_data.py
@@ -0,0 +1,25 @@
+import requests
+
+def get_data(url):
+    '''
+    功能：访问 url 的网页，获取网页内容并返回
+    参数：
+        url ：目标网页的 url
+    返回：目标网页的 html 内容
+    '''
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.text
+
+    except requests.HTTPError as e:
+        print(e)
+        print("HTTPError")
+    except requests.RequestException as e:
+        print(e)
+    except:
+        print("Unknown Error !")
